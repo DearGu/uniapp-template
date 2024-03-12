0d79ec57771238a902b25a6886b12c56238c785a
DearGu <137471594@qq.com>
2024-03-12 15:37:44 +0800

    feat: first commit

---
+++ .eslintrc.js
@@ -0,0 +1,71 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  parser: 'vue-eslint-parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['vue', '@typescript-eslint', 'import'],
+  rules: {
+    camelcase: 'error',
+    'no-var': 'error',
+    allowEmptyCatch: 'off',
+    'prefer-const': 'error',
+    'no-empty': ['error', { allowEmptyCatch: true }],
+    'vue/no-unused-vars': [
+      'error',
+      {
+        ignorePattern: '^_',
+      },
+    ],
+    'vue/multi-word-component-names': 'off',
+    'vue/no-parsing-error': [
+      'error',
+      {
+        'control-character-reference': false,
+      },
+    ],
+    'vue/no-v-html': 'off',
+    'vue/no-setup-props-destructure': 'off',
+    'max-lines': [
+      'warn',
+      {
+        max: 800,
+        skipBlankLines: true,
+        skipComments: true,
+      },
+    ],
+    'import/newline-after-import': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: ['interface', 'typeAlias', 'enum', 'class'],
+        format: ['StrictPascalCase'],
+      },
+    ],
+    '@typescript-eslint/consistent-type-imports': 'error',
+  },
+  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
+    defineExpose: 'readonly',
+    withDefaults: 'readonly',
+  },
+};
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import uni from '@dcloudio/vite-plugin-uni';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [uni()],
+});
